0ffdeed9ff7b03744b3ef50bb99392e352982c27
zhaoyu@hyundai-adrdc.com <zhaoyu@hyundai-adrdc.com>
2023-06-02 17:35:58 +0800

    添加一些方法

--- app/src/main/java/com/example/jniproject/MainActivity.kt
+++ app/src/main/java/com/example/jniproject/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.jniproject
 
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.android.car.zcrash_lib.demo.NativeLib
-import com.android.car.zcrash_lib.demo.Person
+import com.android.car.zcrash_lib.demo.Shared
+import com.android.car.zcrash_lib.demo.Static
+import com.example.jniproject.Utils.Companion.getAbiList
 import com.example.jniproject.databinding.ActivityMainBinding
 
 const val TAG = "MainActivity"
@@ -19,18 +21,18 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val nativeLib = NativeLib()
-        binding.sampleText.text = nativeLib.stringFromJNI()
-        nativeLib.visitField()
-        val persons = nativeLib.createPersons()
+        val static = Static()
+        binding.sampleText.text = static.stringFromJNI()
+        static.visitField()
+        val persons = static.createPersons()
         val names: Array<String> = arrayOf("子鼠", "丑牛", "寅虎", "卯兔", "辰龙")
-        val personNames = nativeLib.getPersons(names)
+        val personNames = static.getPersons(names)
 
         Log.d(
             TAG,
-            "onCreate: name: " + nativeLib.name
-                    + "\nstaticName: " + NativeLib.staticName
-                    + "\ncreatePerson: " + nativeLib.createPerson().toString()
+            "Static: name: " + static.name
+                    + "\nstaticName: " + Static.staticName
+                    + "\ncreatePerson: " + static.createPerson().toString()
         )
         for (person in persons) {
             Log.d(TAG, "createPersons: $person")
@@ -39,5 +41,14 @@ class MainActivity : AppCompatActivity() {
         for (person in personNames) {
             Log.d(TAG, "getPersons: $person")
         }
+
+        Log.d(
+            TAG,
+            "Shared: init: " + Shared.nativeInit(
+                Build.VERSION.SDK_INT,
+                Build.VERSION.RELEASE,
+                getAbiList()
+            )
+        )
     }
 }
